aabe8d8a369021dfe89429aac14856a7feb25e4a
Joe Dukes <joedukes.email@gmail.com>
2019-06-13 11:45:49 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# JS Pagination and Search Project
+
+## Requirements
+
+- [x] Use the filters-example.html file to guide your decision making. Using progressive enhancement, your work should affect the index.html file.
+
+- [x] Since only 10 students should be shown at a time, your programming needs to calculate the number of pages needed and add the appropriate number of links to the bottom of the page.
+
+- [x] Hide all but the first 10 students when the page loads.
+
+- [x] When a user clicks on “2” in the pagination, students 11 through 20 are shown. When a user clicks “3”, students 21 through 30 are shown. And so on. When “6” is clicked 51 through 55 should be shown.
+
+- [x] Using progressive enhancement, add the student search markup as presented in the filters-example.html file to the index.html file.
+
+- [x] Add an event listener to the search button. When the user clicks on the button it should use the text in the search input to filter the results. Searching should be case insensitive. e.g. a search for “Susan” should return results for “susan” and “Susan".
+
+- [x] Users should be able to search by name or e-mail address. And partial matches, like just a first name, should be displayed in the results.
+
+- [x] Search results should also be paginated. For example, if the search returns more than 10 results, those results should be paginated too.
+
+## Extra Credit
+
+- [x] As the user types in the search box, dynamically filter the student listings. In other words, after each letter is typed into the search box, display any listings that match .
+
+- [x] If no matches are found, include a message in the HTML to tell the user there are no matches.
---
+++ js/script.js
@@ -0,0 +1,126 @@
+// Create global variables
+
+const page = document.querySelector(".page");
+const pageHeader = document.querySelector(".page-header");
+const completeStudentList = document.querySelectorAll(".student-item");
+
+const searchDiv = document.createElement("div");
+const searchInput = document.createElement("input");
+const noResultsDiv = document.createElement("div");
+const pageButtonsUl = document.createElement("ul");
+const pageButtonsDiv = document.createElement("div");
+
+let searchString = "";
+let filterStudentList = [];
+const maxStudentsPage = 10;
+
+// Append elements for search
+
+searchDiv.className = "student-search";
+searchInput.placeholder = "Search for students...";
+searchDiv.appendChild(searchInput);
+pageHeader.appendChild(searchDiv);
+
+//Append elements for pagination
+
+pageButtonsDiv.className = "pagination";
+pageButtonsDiv.appendChild(pageButtonsUl);
+page.appendChild(pageButtonsDiv);
+
+// Append element to display if no students match search
+
+noResultsDiv.textContent = "Sorry, no students match your search.";
+noResultsDiv.style.fontSize = "35px";
+noResultsDiv.style.margin = "70px";
+noResultsDiv.style.textAlign = "center";
+noResultsDiv.style.display = "none";
+page.appendChild(noResultsDiv);
+
+// Create the `showPage` function to hide all of the items in the list
+// except for the ten you want to show.
+
+const showPage = (list, button) => {
+  // Set all students to display none
+  for (let i = 0; i < completeStudentList.length; i++) {
+    completeStudentList[i].style.display = "none";
+  }
+  // If no results, display message
+  if (list.length === 0) {
+    noResultsDiv.style.display = "block";
+  }
+  // else display students based on button clicked
+  else {
+    let startIndex = (button - 1) * maxStudentsPage;
+    let endIndex = startIndex + maxStudentsPage;
+    for (let i = startIndex; i < endIndex && i < list.length; i++) {
+      list[i].style.display = "block";
+    }
+    noResultsDiv.style.display = "none";
+  }
+};
+// Create the `appendPageLinks function` to generate, append, and add
+// functionality to the pagination buttons.
+
+const appendPageLinks = studentList => {
+  let numStudents = studentList.length;
+  let numPages = Math.ceil(numStudents / maxStudentsPage);
+  let selectedButton = 1;
+  pageButtonsUl.innerHTML = "";
+
+  showPage(studentList, selectedButton);
+
+  // Create new button for each page
+  for (let i = 0; i < numPages; i++) {
+    let button = document.createElement("li");
+    let anchor = document.createElement("a");
+    anchor.href = "#";
+    // Make first button active
+    if (i === 0) {
+      anchor.className = "active";
+    }
+    // Button text starts at 1 instead of 0
+    anchor.textContent = i + 1;
+    button.appendChild(anchor);
+    pageButtonsUl.appendChild(button);
+  }
+  // Update page when new page button is clicked
+  pageButtonsUl.addEventListener("click", e => {
+    let previousButton = document.querySelector(".active");
+    previousButton.className = "";
+    selectedButton = e.target.textContent;
+    e.target.className = "active";
+    showPage(studentList, selectedButton);
+  });
+};
+
+// Update filterStudent list using search, and refresh
+
+const newSearch = () => {
+  filterStudentList = [];
+  if (searchString.length === 0) {
+    // Use completeStudentList if searchString is empty
+    appendPageLinks(completeStudentList);
+  } else {
+    // For each item in completeStudentList
+    for (let i = 0; i < completeStudentList.length; i++) {
+      // Select student name as a string
+      let name = completeStudentList[i].querySelector("h3").textContent;
+      // If student name contains search substring
+      if (name.includes(searchString)) {
+        // Add student item HTML to filterStudentList
+        filterStudentList.push(completeStudentList[i]);
+      }
+    }
+    appendPageLinks(filterStudentList);
+  }
+};
+
+// Call search() function when input value is changed
+
+searchInput.addEventListener("input", () => {
+  searchString = searchInput.value;
+  newSearch();
+});
+
+// Call when file is initially loaded
+appendPageLinks(completeStudentList);
